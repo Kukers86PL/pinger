b18fe828b6dacb6a2f7955b25f565a93582fa463
Łukasz Grzelak <lukasz.grzelak.86@gmail.com>
2021-08-29 21:43:06 +0200

    First release

---
+++ pinger/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.IO;
+
+namespace pinger
+{
+    public partial class Form1 : Form
+    {
+        private int DOT_SIZE = 300;
+
+        struct Address_status
+        {
+            public String ip;
+            public Boolean isOnline;
+        }
+
+        private List<Address_status> addresses = new List<Address_status>();
+
+        private Boolean isRunning = true;
+
+        void read_config()
+        {
+            String line = "";
+            System.IO.StreamReader file = new System.IO.StreamReader("config.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                Address_status temp;
+                temp.ip = line;
+                temp.isOnline = false;
+                addresses.Add(temp);
+            }
+            file.Close();
+        }
+
+        void check_status()
+        {
+            for (int i = 0; i < addresses.Count(); i++)
+            {
+                Address_status temp = addresses[i];
+
+                Ping pingSender = new Ping();
+                PingReply reply = pingSender.Send(temp.ip);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    temp.isOnline = true;
+                }
+                else
+                {
+                    temp.isOnline = false;
+                }
+
+                addresses[i] = temp;
+            }
+        }
+
+        void store_data()
+        {
+            String line = DateTime.Now.ToString() + ";";
+            StreamWriter file = new StreamWriter("results.txt", true);
+            for (int i = 0; i < addresses.Count(); i++)
+            {
+                if (addresses[i].isOnline)
+                {
+                    line += "1;";
+                }
+                else
+                {
+                    line += "0;";
+                }
+            }
+            file.WriteLine(line);
+            file.Close();
+        }
+
+        void run()
+        {
+            while (isRunning)
+            {
+                check_status();
+                store_data();
+                Thread.Sleep(60000);
+                Invalidate();
+            }            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            read_config();
+            check_status();
+            store_data();
+
+            Thread t = new Thread(run);
+            t.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            this.Width = (DOT_SIZE + 20) * addresses.Count();
+            this.Height = (DOT_SIZE + 40);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+            for (int i = 0; i < addresses.Count(); i++)
+            {
+                if (addresses[i].isOnline)
+                {
+                    e.Graphics.FillEllipse(greenBrush, i * DOT_SIZE, 0, DOT_SIZE, DOT_SIZE);
+                }
+                else
+                {
+                    e.Graphics.FillEllipse(redBrush, i * DOT_SIZE, 0, DOT_SIZE, DOT_SIZE);
+                }
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            isRunning = false;
+        }
+    }
+}
---
+++ pinger/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("pinger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pinger")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("34d588c6-8f19-4728-937d-51780414cf64")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
